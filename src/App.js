d9bea4313b4ddeedad6bcb4754e378176c502593
AjithJohnson33 <ajithjohnson33@gmail.com>
2023-09-28 19:46:37 +0530

    code is pushing for demo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {
+  Button,
+  Form,
+  FormGroup,
+  TextInput,
+} from 'carbon-components-react';
+import { Search } from '@carbon/icons-react';
+
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.github.com/search/repositories?q=${searchTerm}`
+      );
+      setSearchResults(response.data.items);
+    } catch (error) {
+      console.error('Error searching GitHub:', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>GitHub Project Search</h1>
+        <Form>
+          <FormGroup legendText="">
+            <TextInput
+              id="search-input"
+              labelText="Enter project name"
+              placeholder="Search for GitHub projects"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </FormGroup>
+          <Button
+            kind="primary"
+            onClick={handleSearch}
+            renderIcon={Search}
+          >
+            Search
+          </Button>
+        </Form>
+        <div className="search-results">
+          {searchResults.length > 0 && (
+            <h2>Search Results</h2>
+          )}
+          {searchResults.map((result) => (
+            <div key={result.id} className="tile">
+              <div className="tile-above-the-fold-content">
+                <h3>{result.name}</h3>
+                <p>{result.description}</p>
+              </div>
+              <div className="tile-content">
+                <p>Stars: {result.stargazers_count}</p>
+                <p>Language: {result.language}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
---
+++ src/SearchInput.js
@@ -0,0 +1,24 @@
+// SearchInput.js
+import React, { useState } from 'react';
+
+const SearchInput = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = () => {
+    onSearch(searchTerm);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter project name"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchInput;
